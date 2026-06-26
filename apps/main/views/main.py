2ea275712a7a23d1de0bbcf1899460dda9b313c1
mukhagaliaryn <mukhagaliaryn@gmail.com>
2026-06-26 19:15:52 +0500

    Done

--- apps/main/views/main.py
+++ apps/main/views/main.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from core.utils.decorators import anonymous_required
 
 
 # -------------- main page --------------
+@anonymous_required
 def main_view(request):
     context = {}
     return render(request, 'app/page.html', context)
