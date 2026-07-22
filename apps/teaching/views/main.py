40636fab92c1095289363fbb5f40b2f50a6bd362
mukhagaliaryn <mukhagaliaryn@gmail.com>
2026-07-22 18:06:47 +0500

    Done 18:06

--- apps/teaching/views/main.py
+++ apps/teaching/views/main.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.utils.translation import gettext
 from apps.accounts.decorators import teacher_required
 
 
 @teacher_required
 def dashboard_view(request):
-    return render(request, 'teaching/page.html')
+    hour = timezone.localtime().hour
+    if hour < 12:
+        greeting = gettext('Good morning')
+    elif hour < 18:
+        greeting = gettext('Good afternoon')
+    else:
+        greeting = gettext('Good evening')
+
+    return render(request, 'teaching/page.html', {'greeting': greeting})
